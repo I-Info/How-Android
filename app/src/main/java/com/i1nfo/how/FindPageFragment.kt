566612bb0297eebb09e426457ab53da422de3d60
IInfo <info@i1nfo.com>
2022-03-23 23:11:21 +0800

    page fragements

--- app/src/main/java/com/i1nfo/how/FindPageFragment.kt
+++ app/src/main/java/com/i1nfo/how/FindPageFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ActivityPage.newInstance] factory method to
+ * Use the [FindPageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ActivityPage : Fragment() {
+class FindPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ActivityPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity_page, container, false)
+        return inflater.inflate(R.layout.fragment_find_page, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class ActivityPage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ActivityPage.
+         * @return A new instance of fragment FindPage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ActivityPage().apply {
+            FindPageFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/i1nfo/how/UserPageFragment.kt
+++ app/src/main/java/com/i1nfo/how/UserPageFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FindPage.newInstance] factory method to
+ * Use the [UserPageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FindPage : Fragment() {
+class UserPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class FindPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find_page, container, false)
+        return inflater.inflate(R.layout.fragment_user_page, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class FindPage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FindPage.
+         * @return A new instance of fragment UserPage.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FindPage().apply {
+            UserPageFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
