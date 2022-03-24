9a3e5b57e37186770e8e7d61850c0ea110353b1f
IInfo <info@i1nfo.com>
2022-03-24 11:32:29 +0800

    update toolbar

--- app/src/main/java/com/i1nfo/how/MainActivity.kt
+++ app/src/main/java/com/i1nfo/how/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.i1nfo.how
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.Toast
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentContainerView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -17,39 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         // bind navigation actions
         val navBar = findViewById<BottomNavigationView>(R.id.navigationBar)
         navBar.setupWithNavController(navController)
-
-//        navBar.setOnItemSelectedListener { item ->
-//            when(item.itemId) {
-//                R.id.page_main ->{
-//                    navController.navigate(R.id.page_main)
-////                    Toast.makeText(applicationContext,"main",Toast.LENGTH_SHORT).show()
-//                    true
-//                }
-//                R.id.page_activity ->{
-//                    navController.navigate(R.id.page_activity)
-////                    Toast.makeText(applicationContext,"activity",Toast.LENGTH_SHORT).show()
-//                    true
-//                }
-//                R.id.page_message ->{
-//                    navController.navigate(R.id.page_message)
-////                    Toast.makeText(applicationContext,"message",Toast.LENGTH_SHORT).show()
-//                    true
-//                }
-//                R.id.page_user->{
-//                    navController.navigate(R.id.page_user)
-////                    Toast.makeText(applicationContext,"user",Toast.LENGTH_SHORT).show()
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-
 
 
     }
--- app/src/main/java/com/i1nfo/how/MainPageFragment.kt
+++ app/src/main/java/com/i1nfo/how/MainPageFragment.kt
@@ -1,12 +1,15 @@
 package com.i1nfo.how
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainPageFragment : Fragment() {
+
+    private var scrollCounter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +17,34 @@ class MainPageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+
+
+        val scrollView = view.findViewById<NestedScrollView>(R.id.scroll_view)
+        val addButton = view.findViewById<FloatingActionButton>(R.id.add_button)
+
+        // auto hide fab
+        scrollView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+            val deltaY = scrollY - oldScrollY
+            if (deltaY > 0 && addButton.isShown) {
+                scrollCounter += deltaY
+                if (scrollCounter > 100) {
+                    addButton.hide()
+                    scrollCounter = 0
+                }
+            } else if (deltaY < 0 && !addButton.isShown) {
+                scrollCounter += deltaY
+                if (scrollCounter < -100) {
+                    addButton.show()
+                    scrollCounter = 0
+                }
+            }
+        }
     }
 
 }
